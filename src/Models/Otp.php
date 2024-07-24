4b091046f1f91395a24aaa27f8693d7bc8212bc6
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2024-07-25 01:34:07 +0300

    support model:prune command

--- src/Models/Otp.php
+++ src/Models/Otp.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace NjoguAmos\Otp\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
 
 class Otp extends Model
 {
     use HasFactory;
+    use Prunable;
 
     protected $fillable = [
         'identifier',
@@ -27,4 +30,8 @@ class Otp extends Model
         ];
     }
 
+    public function prunable(): Builder
+    {
+        return static::where('expires_at', '<', now());
+    }
 }
--- tests/OtpModelTest.php
+++ tests/OtpModelTest.php
@@ -2,10 +2,23 @@
 
 declare(strict_types=1);
 
-use NjoguAmos\Otp\Models\Otp;
+use NjoguAmos\Otp\Models\Otp as OtpModel;
+
+use function Pest\Laravel\artisan;
 
 it(description: 'encrypts token when saving to database', closure: function () {
-    $otp = Otp::factory()->create(attributes: ['token' => 123456]);
+    $otp = OtpModel::factory()->create(attributes: ['token' => 123456]);
 
     expect(value: $otp->otp)->not->toBe(expected: '123456');
+});
+
+it(description: 'can prune expired otps', closure: function () {
+    $expiredOtp = OtpModel::factory()->create(attributes: ['expires_at' => now()->subMinute()]);
+    $validOtp = OtpModel::factory()->create(attributes: ['expires_at' => now()->addMinute() ]);
+
+    expect(value: OtpModel::count())->toBe(expected: 2);
+
+    artisan(command: 'model:prune', parameters: ['--model' => OtpModel::class]);
+
+    expect(value: OtpModel::count())->toBe(expected: 1);
 });
